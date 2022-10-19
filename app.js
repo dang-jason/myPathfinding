a73a7108ccb2e54b059a878f876909294d7ce4e4
dang-jason <jasondang619@gmail.com>
2022-10-18 22:24:02 -0500

    Starting Pathfinding Project

---
+++ app.js
@@ -0,0 +1,15 @@
+import {bfs} from '/algorithms';
+
+TOTAL_ROWS = 20;
+TOTAL_COLS = 50;
+
+export default class PathfindingVisualizer{
+    constructor(){
+        this.state = {
+            grid: [],
+            mouseIsPressed: false,
+        };
+    }
+
+    
+}
