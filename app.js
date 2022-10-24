1167ce30df7437a32102b7151001fe5bad10569b
dang-jason <jasondang619@gmail.com>
2022-10-23 21:23:12 -0500

    created bfs module for more future algorithms

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//import bfs from './algorithms/bfs';
+import {solveBFS} from './algorithms/bfs.js';
 var isMousePressed = false;
 const TOTAL_ROWS = 20;
 const TOTAL_COLS = 50;
@@ -6,6 +6,7 @@ var START_NODE_ROW = 10;
 var START_NODE_COL = 10;
 var FINISH_NODE_ROW = 10;
 var FINISH_NODE_COL = 40;
+export {TOTAL_ROWS, TOTAL_COLS, START_NODE_ROW, START_NODE_COL, FINISH_NODE_ROW, FINISH_NODE_COL}
 
 const createNode = (col, row) => {
     const node = document.createElement('div');
@@ -47,88 +48,15 @@ const getInitialGrid = (() => {
 })();
 //setting up visual aspects of grid and page
 const setup = (() => {
-    console.log('setup');
-    const page = document.querySelector('body');
+    var page = document.querySelector('body');
     page.addEventListener('mouseup', () => {
         isMousePressed = false;
     });
+
+    document.getElementById('solveBFS').addEventListener('click', () => {solveBFS()});
+    document.getElementById('reset').addEventListener('click', () => {reset()});
     //add change to start and finish nodes using input values! - how to combine input into one element?
 })();
-
-function solveBFS(){
-    document.getElementById(`node-${START_NODE_ROW}-${START_NODE_COL}`).classList.add('node-visited');
-    let queue = [[START_NODE_ROW, START_NODE_COL]];
-    let visited = [];
-    for(let i = 0; i<TOTAL_ROWS; i++) visited[i] = new Array(TOTAL_COLS).fill(false);
-    visited[START_NODE_ROW][START_NODE_COL] = true;
-    let possibleMoves = [
-        [-1,0],
-        [1,0],
-        [0,-1],
-        [0,1]
-    ];
-    let s = 0;
-    solved = false;
-
-
-    while(queue.length){
-        let pos = queue.shift();
-        let r = pos[0];
-        let c = pos[1];
-        let node = document.getElementById(`node-${r}-${c}`);
-        if (node.classList.contains('node-finish')){    
-            solved = true;
-            break;
-        }
-        for (let i = 0; i < possibleMoves.length; i++){
-            let nRow = r+possibleMoves[i][0];
-            let nCol = c+possibleMoves[i][1];
-            if (nRow < 0 || nRow >= TOTAL_ROWS || nCol < 0 || nCol >= TOTAL_COLS || visited[nRow][nCol]){
-                continue;
-            }
-            visited[nRow][nCol] = true;
-            let nNode = document.getElementById(`node-${nRow}-${nCol}`);
-            if(!(nNode.classList.contains('node-visited') || nNode.classList.contains('isWall'))){
-                nNode.dataset.prevnode = `node-${r}-${c}`;
-                s++;
-                queue.push([nRow, nCol]);
-                setTimeout(() => {
-                    nNode.classList.add('node-visited');
-                }, 8*s);
-            }
-        }
-    }
-
-
-    if(!solved){
-        for(let i = 1; i <= 5; i++){
-            if (s < 200 * i){
-                setTimeout( () => {
-                    alert('Path to end cannot be found! I will restart this for you.')
-                    reset();
-                }, (22-(2.5*i))*s);
-                return;
-            }
-
-        }
-    }
-
-    //put the previous node into an array to traceback the shortest path
-    let previous = [`node-${FINISH_NODE_ROW}-${FINISH_NODE_COL}`];
-    let curNode = document.getElementById(`node-${FINISH_NODE_ROW}-${FINISH_NODE_COL}`);
-    while(curNode.dataset.prevnode != "null"){
-        previous.push(curNode.dataset.prevnode);
-        curNode = document.getElementById(curNode.dataset.prevnode);
-    }
-    while(previous.length){
-        let pathNode = document.getElementById(previous.pop());
-        s += 10;
-        setTimeout(() => {
-            pathNode.classList.toggle('node-visited');
-            pathNode.classList.add('node-path');
-        }, 8*(s));
-    }
-}
 
 function reset(){
     //clear timeoutIDs
